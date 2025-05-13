198b5aebc24bf79533fc3d461941f26bf3d4f5f2
Bill Wilder <codingoutloud@gmail.com>
2025-05-13 12:01:01 -0400

    first version - works

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+   // Do not interfere with MCP - send all logs to go to stderr
+   consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class WebSearchTool
+{
+   [McpServerTool]
+   [Description("Searches the web. Similar to Google or Bing search.")]
+   public static async Task<string> SearchWeb(string query)
+   {
+      // await Task.Delay(500);
+      // return "answer hello";
+      return await BraveSearchService.SearchWeb(query);
+   }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# brave-search-mcp
-MCP server that exposes Google Web Search API. Uses STDIO approach. Written in C# using the Semantic Kernel SDK.
+# Brave Search MCP
+
+MCP server that exposes Brave Web Search API. Uses STDIO approach. 
+
+Written in C# using the Semantic Kernel SDK.
+
+Caller provides the API key to the [Brave Web Search API](https://api-dashboard.search.brave.com/app/documentation/web-search/get-started).
+
+One way to test: `npx @modelcontextprotocol/inspector dotnet run`
